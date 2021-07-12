9f24387304ad9a460d87a81b14cd48afccee4661
karnhao <karn0888422952@gmail.com>
2021-07-12 20:46:36 +0700

    Fix 7/12/2021 8:46

--- widget/ts/widget.js
+++ widget/ts/widget.js
@@ -761,6 +761,7 @@ function main_shortcut() {
                         throw new Error("ไม่มีข้อมูล url ของการประชุมในวิชานี้");
                     }
             }
+            break;
         default:
             Script.setShortcutOutput("Error : มีบางอย่างผิดพลาด");
             break;
